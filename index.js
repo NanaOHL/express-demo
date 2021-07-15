d0ec63eb4960e0be55fe8b84b731725093b0c847
Nana Hughes-Lartey <nana@spctr.co.uk>
2021-07-15 09:49:39 +0100

    chucked brads video about 1hrs mark  https://youtu.be/L72fhGm1tfE

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const path = require('path');
-const logger = require('./middleware/logger')
-
-const members = require('./Members');
+const exphbs = require('express-handlebars');
+// const logger = require('./middleware/logger')
 
 const app = express();
 
-
-
 //init middleware
-app.use(logger)
+// app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-// Get members
-app.get('/api/members', (req, res) => res.json(members));
-
+//BOdy Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extented: false}));
 
 
 // Set Static folder
 app.use(express.static(path.join(__dirname,'public')));
 
-
+//Member API Router
+app.use('/api/members', require('./routes/api/members'));
+ 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); 
